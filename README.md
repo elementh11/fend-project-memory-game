ff513c343ca7ff342d694c333450ab229c98e201
Adrian Popescu <elementh11@gmail.com>
2018-06-19 16:08:56 -0400

    Add README.md and code formatting

--- README.md
+++ README.md
@@ -2,19 +2,28 @@
 
 ## Table of Contents
 
+* [Description](#description)
 * [Instructions](#instructions)
 * [Contributing](#contributing)
 
+## Description
+
+The memory game is a project part of my Udacity FEND Nano Degree. It is a complete browser-based card matching game (also know as Concentration).
+
+The project rubric is available here:
+[ Memory Game project rubric](https://review.udacity.com/#!/rubrics/591/view).
+
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+To play the game:
+1. Start clicking on cards (the timer will start);
+2. Try, using your memory, to match the cards (the moves are counted);
+3. The game is over when all cards are matched, you will get a star rating:
+   - 3 stars for less than 14 moves
+   - 2 stars for less than 20 moves
+   - 1 star for more than 20 moves
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+The project has 2 contributors: me and Richard from Udacity, who did the initial commit for the starter code.
+I also used some CSS styling for the modal from [W3Schools](https://www.w3schools.com/howto/howto_css_modals.asp).
--- js/app.js
+++ js/app.js
@@ -2,22 +2,22 @@
  * Create a list that holds all of your cards
  */
  let cardsList = [
-   "diamond",
-   "diamond",
-   "paper-plane-o",
-   "paper-plane-o",
-   "anchor",
-   "anchor",
-   "bolt",
-   "bolt",
-   "cube",
-   "cube",
-   "leaf",
-   "leaf",
-   "bomb",
-   "bomb",
-   "bicycle",
-   "bicycle"
+   'diamond',
+   'diamond',
+   'paper-plane-o',
+   'paper-plane-o',
+   'anchor',
+   'anchor',
+   'bolt',
+   'bolt',
+   'cube',
+   'cube',
+   'leaf',
+   'leaf',
+   'bomb',
+   'bomb',
+   'bicycle',
+   'bicycle'
  ];
 
  let openCardsList = [];
@@ -29,7 +29,6 @@
  let minutes = 0;
  let elapsedTime = 0;
 
-
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -39,7 +38,7 @@
 let shuffledCards = shuffle(cardsList);
 function shuffleCards() {
    for (let i = 0; i < 16; i++) {
-       $(".deck").append($('<li class="card"><i class="fa fa-' + shuffledCards[i] + '"></i></li>'))
+       $('.deck').append($('<li class="card"><i class="fa fa-' + shuffledCards[i] + '"></i></li>'))
    };
 }
 shuffleCards();
@@ -47,7 +46,6 @@ shuffleCards();
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -55,7 +53,6 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
@@ -71,33 +68,35 @@ function shuffle(array) {
  */
 
 function playGame() {
-  addCardListener();
+   addCardListener();
 }
 
-
-
+/**
+* @description Sets the event listener for a card
+* on click starts the timer, display the card symbol,
+* and add card to openCardsList
+*/
 function addCardListener() {
-  $(".card").on("click", function() {
-
-    clickCounter++;
-    if (clickCounter === 1) {
-      timerOn();
+   $('.card').on('click', function() {
+       clickCounter++;
+       if (clickCounter === 1) {
+          timerOn();
     }
-
-    const myCard=$(this);
+    let myCard=$(this);
     displayCard(myCard);
     addCardToList(myCard);
-
+/* check if cards match
+   further action according to the description above */
     if (openCardsList.length ===2) {
         if (openCardsList[0][0].firstChild.className === openCardsList[1][0].firstChild.className) {
-          lockCard();
-          matches++;
-          openCardsList=[];
+           lockCard();
+           matches++;
+           openCardsList=[];
         } else {
             turnOffClick();
             setTimeout(function () {
-              turnOnClick();
-              hideCard();
+               turnOnClick();
+               hideCard();
             }, 500);
             openCardsList=[];
         };
@@ -105,70 +104,105 @@ function addCardListener() {
         starScore();
     };
     checkScore();
-  });
+   });
 }
 
-
-
-
+/** @description Starts the Game
+*/
 playGame();
-$(".restart").on("click", function() {
-  playGame(location.reload());
+$('.restart').on('click', function() {
+   playGame(location.reload());
 });
 
-
+/** @description Displays the card symbol
+*/
 function displayCard(myCard) {
-  myCard.addClass("open show");
+   myCard.addClass('open show');
 }
+
+/** @description Hides the card symbol
+*/
 function hideCard() {
-  $('.deck').find('.open').removeClass('open show');
+   $('.deck').find('.open').addClass('nomatch');
+   setTimeout(function () {
+     $('.deck').find('.open').removeClass('open show nomatch');
+   }, 350);
 }
+
+/** @description Add card to a *list* of "open" cards
+*/
 function addCardToList(myCard) {
-  openCardsList.push(myCard);
+   openCardsList.push(myCard);
 }
+
+/** @description Lock matching cards into open position
+*/
 function lockCard() {
-  $('.deck').find('.open').addClass('match');
+   $('.deck').find('.open').addClass('match');
 }
+
+/** @description Turn off mouse click to prevent
+* opening more than 2 cards
+*/
 function turnOffClick() {
-  $(".card").addClass("noclick");
+   $('.card').addClass('noclick');
 }
+
+/** @description Turn mouse click back on
+*/
 function turnOnClick() {
-  $(".card").removeClass("noclick");
+   $('.card').removeClass('noclick');
 }
+
+/** @description increment the move counter
+* and display it on the page
+*/
 function incrementMoves() {
-  moves++;
-  $('.moves').text(`${moves}`);
+   moves++;
+   $('.moves').text(`${moves}`);
 }
+
+/** @description check if all cards have matched
+* and display the modal
+*/
 function checkScore() {
-  if (matches === 8) {
-    clearInterval(elapsedTime);
-    $(".modal").css("display", "flex");
-    $(".button").on("click", function() {
-      playGame(location.reload())});
-    $(".close").on("click", function() {
-      $(".modal").css("display", "none");
-      $('.deck .card').addClass("noclick");
+   if (matches === 8) {
+      clearInterval(elapsedTime);
+      $('.modal').css('display', 'flex');
+      $('.button').on('click', function() {
+        playGame(location.reload())
+      });
+      $('.close').on('click', function() {
+        $('.modal').css('display', 'none');
+        $('.deck .card').addClass('noclick');
       });
   }
 }
+
+/** @description display the star rating
+*/
 function starScore() {
-  if (moves >= 14) {
-    $('.fa-star').eq(1).css("display", "none");
-    rating = 2;
-  }
-  if (moves >= 20) {
-    $('.fa-star').eq(2).css("display", "none");
-    rating = 1;
-  };
-  $('.rating').text(`${rating}`);
+   if (moves >= 14) {
+      $('.fa-star').eq(1).css('display', 'none');
+      rating = 2;
+   }
+   if (moves >= 20) {
+      $('.fa-star').eq(2).css('display', 'none');
+      rating = 1;
+   };
+   $('.rating').text(`${rating}`);
 }
+
+/** @description increment the time
+* and display it on the page
+*/
 function timerOn() {
-  elapsedTime = setInterval(function() {
-    $(".timer").text(`${minutes}:${seconds}`);
-    seconds++;
-    if (seconds === 60) {
-      minutes++;
-      seconds = 0;
-    }
-  }, 1000);
+   elapsedTime = setInterval(function() {
+      $('.timer').text(`${minutes}:${seconds}`);
+      seconds++;
+      if (seconds === 60) {
+        minutes++;
+        seconds = 0;
+      }
+   }, 1000);
 }
